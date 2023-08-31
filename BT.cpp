9b83299c965f020878fd7036adae2ef89732c6a1
Aswini reddy4571 <122337756+Aswinireddy4571@users.noreply.github.com>
2023-08-31 12:16:54 +0530

    Add files via upload

---
+++ BT.cpp
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Define the structure for a binary tree node
+struct TreeNode {
+    int data;
+    struct TreeNode* left;
+    struct TreeNode* right;
+};
+
+// Function to create a new binary tree node
+struct TreeNode* createNode(int data) {
+    struct TreeNode* newNode = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+// Function to insert a value into the binary tree
+struct TreeNode* insert(struct TreeNode* root, int data) {
+    if (root == NULL) {
+        return createNode(data);
+    }
+
+    if (data < root->data) {
+        root->left = insert(root->left, data);
+    } else {
+        root->right = insert(root->right, data);
+    }
+
+    return root;
+}
+
+// Function to traverse the binary tree in-order
+void inorderTraversal(struct TreeNode* root) {
+    if (root != NULL) {
+        inorderTraversal(root->left);
+        printf("%d ", root->data);
+        inorderTraversal(root->right);
+    }
+}
+
+int main() {
+    struct TreeNode* root = NULL;
+
+    int values[] = {2, 1, 4, 6, 9, 16};
+    int numValues = sizeof(values) / sizeof(values[0]);
+
+    for (int i = 0; i < numValues; i++) {
+        root = insert(root, values[i]);
+    }
+
+    printf("In-order traversal of the binary tree: ");
+    inorderTraversal(root);
+    printf("\n");
+
+    return 0;
+}
+
